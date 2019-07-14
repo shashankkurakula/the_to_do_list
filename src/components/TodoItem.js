37d24fc1e1a41e61c2e9e9edd13c6d8bc18707e3
shashankkurakula <shashankkurakula96@gmail.com>
2019-07-14 19:12:51 +0530

    Intial commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class TodoItem extends Component {
+    getStyle = () => {
+
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+        // if (this.props.todo.completed) {
+        //     return {
+        //         textDecoration: 'line-through'
+        //     }
+        // } else {
+        //     return {
+        //         textDecouration: 'none'
+        //     }
+        // }
+    }
+
+    render() {
+        const { id, title } = this.props.todo;
+        return (
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+                    {title}
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>X</button>
+                </p>
+            </div>
+        )
+    }
+}
+//we can use 'this' in render and others bcz they are part of component, and here markComplete() is custom method, so we can use onChange={this.markComplete.bind(this)} or can use arrow function
+
+// <div style={{ backgroundColor: 'red' }}>   inline styles
+
+// < div style={itemStyle} >    inline using a variable
+// const itemStyle = {
+//     backgroundColor: 'red'
+// }
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 8px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+
+}
+
+export default TodoItem
---
+++ src/components/Todos.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import TodoItem from './TodoItem'
+
+
+
+export class Todos extends React.Component {
+    render() {
+        return this.props.todos.map((todos) => (
+            //mapping forms a list and list should have a key (just shows warnings)
+            <TodoItem key={todos.id} todo={todos} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
+        ))
+    }
+}
+// proptypes are like validations for properties a component should have
+
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+export default Todos
